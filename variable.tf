6e79a4907583e3d94ba83a5e5cabc4451e19d0c4
monikaranjan12 <monikaranjan8252@gmail.com>
2025-11-03 15:51:42 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,34 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+}
 
+variable "environment" {
+  description = "Environment name (e.g., dev, prod)"
+  type        = string
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDRs for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDRs for private subnets"
+  type        = list(string)
+}
+
+variable "database_subnet_cidrs" {
+  description = "List of CIDRs for database subnets"
+  type        = list(string)
+}
+
+variable "enable_nat_gateway" {
+  description = "Enable NAT Gateway for private subnets"
+  type        = bool
+}
